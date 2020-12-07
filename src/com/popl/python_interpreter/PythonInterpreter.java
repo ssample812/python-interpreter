4565ae6ba8ccbd47fb39ca0128fead16190736eb
Samantha Sample <ssample812@gmail.com>
2020-12-06 18:15:07 -0600

    create main

---
+++ src/com/popl/python_interpreter/PythonInterpreter.java
@@ -0,0 +1,10 @@
+package com.popl.python_interpreter;
+
+public class PythonInterpreter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
