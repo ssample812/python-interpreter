4dc0aac74804b7ebe1aa1d3eed44d1c04737f49a
Samantha Sample <ssample812@gmail.com>
2020-12-15 16:39:31 -0600

    print

--- src/com/popl/python_interpreter/PythonInterpreter.java
+++ src/com/popl/python_interpreter/PythonInterpreter.java
@@ -1,13 +1,18 @@
 package com.popl.python_interpreter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+
+
 public class PythonInterpreter {
+
+	private static HashMap<String, String> variables = new HashMap<>();
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -15,6 +20,11 @@ public class PythonInterpreter {
 		String fileName;
 		File pythonFile;
 		List<String> fileLines = new ArrayList<>();
+
+		variables.put("charmender_attack", "knife");
+		variables.put("squirtle_HP", "5");
+		variables.put("num", "3");
+		variables.put("name", "Sam");
 
 		System.out.println("Enter the name of your Python file (ex: script.py): ");
 		fileName = scan.nextLine();
@@ -27,6 +37,7 @@ public class PythonInterpreter {
 				if (!data.matches("#.*")) {
 					fileLines.add(data);
 				}
+				print(data);
 			}
 			fileScanner.close();
 		} catch (FileNotFoundException e) {
@@ -34,11 +45,33 @@ public class PythonInterpreter {
 			e.printStackTrace();
 		}
 
-		for(String line : fileLines) {
-			System.out.println(line);
-		}
+		// for(String line : fileLines) {
+		// 	System.out.println(line);
+		// }
 
 		scan.close();
 	}
 
+	private static void print(String line) {
+		String str = line.substring(line.indexOf("(") + 1, line.length() - 1);
+		String str_segments[] = str.split("\\+");
+
+		String output = "";
+
+		for (String seg: str_segments){
+			if (seg.charAt(0) == '\"') {
+				output += seg.substring(1, seg.length() - 1);
+			} else if (seg.startsWith("str")) {
+				String var = seg.substring(4, seg.length() - 1);
+				output += variables.get(var);
+			} else {
+				output += variables.get(seg);
+			}
+		}
+		System.out.println(output);
+	}
+
+	private static boolean evaluate(String line) {
+		return true;
+	}
 }
